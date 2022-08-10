15f3aef0ca5f2727782e7f3ee5035542958ae04d
ArnM <amoyal@uni.pe>
2022-08-10 17:08:28 -0500

    Add project files.

---
+++ BethanyPieShop/Program.cs
@@ -0,0 +1,17 @@
+using BethanyPieShop.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapDefaultControllerRoute();
+app.Run();
---
+++ BethanyPieShop/ViewModels/PieListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BethanyPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; }
+        public string? CurrentCategory { get; }
+        public PieListViewModel(IEnumerable<Pie> pies, string? currentCategory)
+        {
+            Pies = pies;
+            CurrentCategory = currentCategory;
+        }
+    }
+}
